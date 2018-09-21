a2ddddab92dcd80f2ae88c2f82c50022734e0769
Marco Benedetti <marcobenedetti@MacBook-Pro-di-Marco.local>
2018-09-21 05:35:18 +0200

    Inserimento commenti

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
-import React, { Component } from 'react';
-import { Button,FormGroup,Form,Row,Container,Col} from 'reactstrap';
+/**
+ * Hyperting Test
+ * @author Benedetti Marco <marco.benedetti.g@gmail.com>
+ */
+import React, { Component } from 'react';//import dei componenti di react
+import { Button,FormGroup,Form,Row,Container,Col} from 'reactstrap';//import dei componenti di reactstrap
 import './App.css';
-import axios from 'axios'
-
-class App extends Component {
+import axios from 'axios'//import della libreria Axios
+/**
+ * Classe App
+ * @class
+ */
+class App extends Component {//inizializzazione classe di default
   constructor () {
     super()
     this.state = {}
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this)//binding funzione "handleClick" per abilitare 'this'
   }
- 
+ /**
+ * Funzione per cancellare il localstorage
+ * @function clearClick
+ */
   clearClick()
   {
-    localStorage.clear("frasi_salvate");
-    window.location.reload();
+    localStorage.clear("frasi_salvate");//clear del localstorage
+    window.location.reload();//reload pagina
   }
 
+/**
+ * Funzione per avviare il fetch
+ * @function handleClick
+ */
   handleClick () {
-    axios.get('https://api.chucknorris.io/jokes/random')
-      .then(response => this.setState({frase: response.data.value}))
-      .then(result => this.onSetResult());
+    axios.get('https://api.chucknorris.io/jokes/random')//fetch api
+      .then(response => this.setState({frase: response.data.value}))//salvataggio frase
+      .then(result => this.onSetResult());//chiamata ad avvenuto fetch
   }
-
+/**
+ * Funzione di avvenuto "fetching"
+ * @function onSetResult
+ * @this handleClick
+ */
   onSetResult = () => {
-    var frasi=[];
+    var frasi=[];//inizializzazione var di appoggio
     if(localStorage.getItem("frasi_salvate")!==null)
-    frasi.push(localStorage.getItem("frasi_salvate"));
-    frasi.push(this.state.frase);
-    localStorage.setItem("frasi_salvate", frasi);
+    frasi.push(localStorage.getItem("frasi_salvate"));//salvataggio frasi precedenti
+    frasi.push(this.state.frase);//inserimento nuova frase
+    localStorage.setItem("frasi_salvate", frasi);//salvataggio in memoria di tutte le frasi
     window.location.reload();
   }
-    
+  /**
+ * Render del risultato
+ * @function render
+ */  
   render () {
     return (
       <Form>
